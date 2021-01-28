af57d5673b21ed9ba5297b818ee9d6d3f15dcc08
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2021-01-27 19:17:23 -0500

    Finsihed simple HTTP homework

---
+++ src/htmlResponses.js
@@ -0,0 +1,20 @@
+const fs = require('fs'); // pull in the file system modile
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getPage2 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
+};
+
+// export the functions so server.js can use them
+module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
